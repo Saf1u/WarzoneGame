747feb09f9d7caa974385d5738f16adb81e584fd
saf1u <a_safwa@live.cocnordia.ca>
2021-11-10 20:22:24 -0500

    fresh

--- Orders.fwd.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Safwan Ahmed on 2021-11-07.
-//
-
-#ifndef WARZONEGAME2_ORDERS_FWD_H
-#define WARZONEGAME2_ORDERS_FWD_H
-class Order;
-class OrderList;
-#endif //WARZONEGAME2_ORDERS_FWD_H
--- Player.fwd.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Safwan Ahmed on 2021-11-07.
-//
-
-#ifndef WARZONEGAME2_PLAYER_FWD_H
-#define WARZONEGAME2_PLAYER_FWD_H
-class Player;
-#endif //WARZONEGAME2_PLAYER_FWD_H
